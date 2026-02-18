981cacba66a0fa72661f73fc6617031a5c0dce64
bittinikkari1 <groytopia@gmail.com>
2026-02-18 20:11:44 +0200

    kuvien lukufunktio lisätty menuun + kuvajutut kansio lisätty johon apuohjelmia

---
+++ jsonlueaktiiviset.php
@@ -0,0 +1,25 @@
+<?php
+//http:localhost:81/Lopullinen/jsonlue.php
+mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
+try{
+    $yhteys=mysqli_connect("db", "root", "password", "plantti");
+}
+catch(Exception $e){
+    print "Yhteysvirhe";
+    exit;
+}
+$tulos=mysqli_query($yhteys, "select * from menu_items where aktiivinen=1");
+$lista = [];
+while ($rivi=mysqli_fetch_object($tulos)){
+    $kategoriat=new class{};
+    $kategoriat->id=$rivi->id;
+    $kategoriat->kategoria_id=$rivi->kategoria_id;
+    $kategoriat->annos=$rivi->annos;
+    $kategoriat->aineet=$rivi->aineet;
+    $kategoriat->kuvaus=$rivi->kuvaus;
+    $lista[]=$kategoriat;
+}
+mysqli_close($yhteys);
+print json_encode($lista);
+
+?>
